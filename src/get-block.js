674bb3eaaa507ff826a0a6e1b8e8f11dae06515e
Dan Forbes <dan@danforbes.dev>
2020-09-04 00:06:10 -0700

    Prettier

--- src/get-block.js
+++ src/get-block.js
@@ -1,26 +1,37 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { formatNumber } from '@polkadot/util';
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { formatNumber } from "@polkadot/util";
 
-import getOpts from './get-opts';
+import getOpts from "./get-opts";
 
 const opts = getOpts();
-let blockNumber = opts['block-number'] ? parseInt(opts['block-number']) : 0;
-let rpcProvider = opts['rpc-provider'] || 'wss://kusama-rpc.polkadot.io';
+let blockNumber = opts["block-number"] ? parseInt(opts["block-number"]) : 0;
+let rpcProvider = opts["rpc-provider"] || "wss://kusama-rpc.polkadot.io";
 
 (async () => {
   const wsProvider = new WsProvider(rpcProvider);
   const api = await ApiPromise.create({ provider: wsProvider });
 
-	const blockStart = Date.now();
+  const blockStart = Date.now();
 
-	try {
-		const hash = await api.rpc.chain.getBlockHash(blockNumber);
-		const events = await api.query.system.events.at(hash);
-		console.log(formatNumber(blockNumber).padStart(10), `${`${Date.now() - blockStart}`.padStart(7)}ms`,  events.map(({ event: { data: { method, section } } }) => `${section}.${method}`).join(', '));
-	} catch (e) {
-		console.log(e)
-	}
-
+  try {
+    const hash = await api.rpc.chain.getBlockHash(blockNumber);
+    const events = await api.query.system.events.at(hash);
+    console.log(
+      formatNumber(blockNumber).padStart(10),
+      `${`${Date.now() - blockStart}`.padStart(7)}ms`,
+      events
+        .map(
+          ({
+            event: {
+              data: { method, section },
+            },
+          }) => `${section}.${method}`
+        )
+        .join(", ")
+    );
+  } catch (e) {
+    console.log(e);
+  }
 
   process.exit();
 })();
